3eed4e7b44ef62531084f8ced396d5bb81724a24
Andy Bitz <artzbitz@gmail.com>
2021-08-21 18:10:27 +0200

    Add favicon

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,6 +8,7 @@ export function Layout({ children }: { children: ReactNode; }) {
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>Floating Point to Binary</title>
+				<link rel="icon" type="image/png" href="/favicon.png" />
 			</Head>
 			{children}
 		</main>
