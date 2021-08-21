7d563cc4c12c36c2033269596e48381db0ed661d
Andy Bitz <artzbitz@gmail.com>
2021-08-21 17:20:21 +0200

    Add link

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,7 @@ export function Header() {
 		<Container className={styles.header}>
 			<h1 className={styles.headline}>Floating Point to Binary</h1>
 			<p className={styles.text}>
-				Convert floating point numbers to their binary representation according to the IEEE 754 standard.
+				Convert floating point numbers to their binary representation according to the <a target="_blank" href="https://en.wikipedia.org/wiki/IEEE_754">IEEE 754</a> standard.
 			</p>
 		</Container>
 	);
